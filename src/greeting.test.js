5ecffe3b31bfbbfe577890e5ea17d8a73f4c3a6e
Thomas Ari Bech <thomasb17@ru.is>
2018-10-22 13:21:45 +0000

    this is cool

--- src/greeting.test.js
+++ src/greeting.test.js
@@ -1,5 +1,5 @@
 const greeting = require("./greeting");
 
 test("returns greeting with custom name", () => {
-	expect(greeting("Bei")).toBe("Hello, Bei!!");
+	expect(greeting("Be")).toBe("Hello, Be!!");
 });
